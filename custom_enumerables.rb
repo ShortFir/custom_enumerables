dea250f58c08bbf860da8d4258df401145c6f012
ShortFir <shortfir@protonmail.com>
2021-05-24 16:54:16 +1000

    my_each, my_each_with_index

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/For
 # Custom Enumerables
 module Enumerable
   def my_each
-    yield
+    for value in self do
+      yield(value)
+    end
   end
 
   def my_each_with_index
-    yield
+    index = 0
+    for value in self do
+      yield(value, index)
+      index += 1
+    end
   end
 
   def my_select
@@ -38,3 +45,25 @@ module Enumerable
     yield
   end
 end
+# rubocop:enable Style/For
+
+numbers = [1, 2, 3, 4, 5]
+num_intp = %W[#{numbers[0]} #{numbers[1]} #{numbers[2]} #{numbers[3]} #{numbers[4]}]
+words = %w[one two three four five]
+
+puts 'each :VS: my_each'
+
+numbers.each { |value| puts "Value: #{value}" }
+numbers.my_each { |value| puts "Value: #{value}" }
+
+puts 'each_with_index :VS: my_each_with_index'
+
+words.each_with_index do |value, index|
+  puts "Value: #{value.to_s.ljust(5)} Index: #{index}"
+end
+words.my_each_with_index do |value, index|
+  puts "Value: #{value.to_s.ljust(5)} Index: #{index}"
+end
+
+puts 'array interpolation'
+num_intp.each { |value| puts "Value: #{value}" }
